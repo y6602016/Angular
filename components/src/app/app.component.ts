bdc2fb75c801e5e207f6d13d8ff47f13c1461fdc
洪啟睿 <y6602016@gmail.com>
2021-12-23 14:41:57 -0500

    post application

--- components/src/app/app.component.ts
+++ components/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'components';
+  posts = [
+    {
+      img: '',
+      author: 'John',
+      title: 'Title # 1'
+    },
+    {
+      img: '',
+      author: 'Mike',
+      title: 'Title # 2'
+    },
+    {
+      img: '',
+      author: 'Jane',
+      title: 'Title # 3'
+    }
+  ]
 }
