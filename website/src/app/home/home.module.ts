dfb447c9c567dd51afebdf6045022e20045607d8
洪啟睿 <y6602016@gmail.com>
2021-12-26 15:34:23 -0500

    add routes

---
+++ website/src/app/home/home.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeMainComponent } from './home-main/home-main.component';
+
+
+@NgModule({
+  declarations: [
+    HomeMainComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ],
+  exports: [
+    HomeMainComponent
+  ]
+})
+export class HomeModule { }
