ddc056162f1f9e88a29efce99beb6cc641549b81
洪啟睿 <y6602016@gmail.com>
2021-12-27 13:03:51 -0500

    service

---
+++ cocktails/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { ResultsComponent } from './results/results.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent,
+    ResultsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
