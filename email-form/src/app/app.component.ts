cec71626027fd05962b8399f2f2c6b934c3eca3e
洪啟睿 <y6602016@gmail.com>
2021-12-30 15:36:06 -0500

    update

--- email-form/src/app/app.component.ts
+++ email-form/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'email-form';
+  email = '';
+  handleSubmit() {
+    console.log(this.email);
+  }
 }
